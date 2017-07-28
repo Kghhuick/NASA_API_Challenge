7c42e7da11b3eb5aab7bc41fbf284bb129952519
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-07-28 15:06:34 +0200

    add button and event which loads next 6 photos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function()	{
 
 var url = "https://api.nasa.gov/planetary/apod?api_key=01Wot9xYzTu3re5cf3w4HMAhwcrdntrVJAAvadzv"
+var urlA ="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=01Wot9xYzTu3re5cf3w4HMAhwcrdntrVJAAvadzv"
 
 $.ajax({
 				method:	"GET",
@@ -15,9 +16,6 @@ $.ajax({
         $(".section_one_div").css('background-image', 'url(' + a + ')');
 
 });
-
-
- var urlA ="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=01Wot9xYzTu3re5cf3w4HMAhwcrdntrVJAAvadzv"
 
  $.ajax({
  				method:	"GET",
@@ -36,32 +34,56 @@ $.ajax({
 			var newDivman = $("<div>");
       sectionTwo.prepend(newDivman);
 			newDivman.css('background-image', 'url(' + c + ')');
-
-
-
-    }
+   }
  });
+
+
+
+
+ $("button").on("click",function(){
+	 $.ajax({
+					 method:	"GET",
+					 url:	urlA,
+					 dataType:	"json"
+	 }).done(function(response)	{
+
+
+			 console.log(response);
+
+			 for(var i = 6; i < 12; i++){
+				var c = response.photos[i].img_src;
+			 console.log(c);
+
+			 var sectionTwo = $(".div_two");
+			 var newDivman = $("<div>");
+				sectionTwo.prepend(newDivman);
+			 newDivman.css('background-image', 'url(' + c + ')');
+		 }
+	 });
+ });
+
+
+
+
+
 
   var cnt = 0;
 
   $(".div_two").on("click","div",function(){
-		if(cnt % 2 === 0) {
+	 if(cnt % 2 === 0) {
   $(".div_two div").hide();
 	var z = $(this);
 	z.css({"width":"100%","height":"100%","margin":"0 0 0 0"});
   $(this).show();
   cnt ++;
-	console.log(cnt);
-}else {
 
-	 $(".div_two div").show();
-	 $(".div_two div").css({"width":"20%","height":"20%"});
+} else {
+   $(".div_two div").show();
+	 $(".div_two div").css({"width":"25%","height":"200px","margin":"20px 20px 20px 20px"});
 	 cnt++;
 }
-	 });
 
-
-
+});
 
 
 
